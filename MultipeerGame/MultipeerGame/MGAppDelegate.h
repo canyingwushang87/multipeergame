e56889f52d446ce7af5e2ff665025309d092bc8e
canyingwushang <527519486@qq.com>
2014-06-28 16:12:30 +0800

    add Main VC
    add

--- MultipeerGame/MultipeerGame/MGAppDelegate.h
+++ MultipeerGame/MultipeerGame/MGAppDelegate.h
@@ -11,5 +11,6 @@
 @interface MGAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *naviVC;
 
 @end
