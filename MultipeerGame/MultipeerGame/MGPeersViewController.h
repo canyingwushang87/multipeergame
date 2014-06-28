f6cc0ddff545f895202d389a66fedd0707e9b2b7
gutiefeng <gutiefeng@gmail.com>
2014-06-28 16:30:00 +0800

    Peers VC
    Peers VC

---
+++ MultipeerGame/MultipeerGame/MGPeersViewController.h
@@ -0,0 +1,12 @@
+//
+//  MGRoomViewController.h
+//  MultipeerGame
+//
+//  Created by Admin on 14-6-28.
+//  Copyright (c) 2014年 baidubox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGPeersViewController : UIViewController
+@end
